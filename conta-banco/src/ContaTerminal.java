03cbcdd7a20273487ef24be7cc08e2fce64e9e78
Fernando Souza <fernandoantonio.souza@outlook.com>
2024-08-18 22:44:26 -0300

    desafio implementado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,33 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 0;
+
+        System.out.println("Por favor, digite seu nome e aperte ENTER:");
+        nomeCliente = input.nextLine();
+
+        System.out.println("Por favor, insira o número da conta e aperte ENTER:");
+        numero = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Por favor, insira o número da agência e aperte ENTER:");
+        agencia = input.nextLine();
+
+        System.out.printf("Seu saldo atual é de R$ %.2f\n", saldo);
+        System.out.println("Por favor, informe o valor a depositar ENTER:");
+        saldo += input.nextDouble();
+        input.nextLine();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco,"
+                + " sua agência é %s, conta %d e seu saldo %.2f já está disponível", nomeCliente, agencia, numero, saldo);
+
     }
 }
